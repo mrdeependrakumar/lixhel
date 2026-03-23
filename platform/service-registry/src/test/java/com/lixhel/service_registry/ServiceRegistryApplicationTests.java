d941198493cdf098c7018035b6c02ae47e4df551
mrdeependrakumar <mrdeependrak@gmail.com>
2026-03-24 01:18:59 +0530

    Created service-registry and config-server

---
+++ platform/service-registry/src/test/java/com/lixhel/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lixhel.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
